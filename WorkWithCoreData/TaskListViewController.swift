634a2749e4e5369eac67b35ad48ea01390202118
Valeria Khristinina <valeria.khristinina@gmail.com>
2020-03-11 12:33:12 +0100

    fix

--- WorkWithCoreData/TaskListViewController.swift
+++ WorkWithCoreData/TaskListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TaskListViewController.swift
 //  WorkWithCoreData
 //
 //  Created by Valeria Khristinina on 11/03/2020.
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TaskListViewController: UITableViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
 
 
